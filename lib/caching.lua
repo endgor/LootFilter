6d449b4b52ce9886a1a2aca2ec32d7046ad37472
Ender Görgülü <ender@gorgulu.se>
2026-02-09 22:10:17 +0100

    Revert "Gate remaining print calls behind silence mode check"
    This reverts commit 1a4f0921933c0334b8280ed3462ea55d241fab31.

--- lib/caching.lua
+++ lib/caching.lua
@@ -43,9 +43,7 @@ function LootFilter.processCaching()
 			else
 				calculatedValue = tonumber(LootFilter.cleanList[1]["value"]*LootFilter.cleanList[1]["stack"]);
 			end;
-			if (not LootFilterVars[LootFilter.REALMPLAYER].silent) then
-				LootFilter.print(LootFilter.cleanList[1]["link"].." "..LootFilter.Locale.LocText["LTWasDeleted"]..": "..LootFilter.Locale.LocText["LTItemLowestValue"].." ("..calculatedValue / 10000 ..")");
-			end;
+			LootFilter.print(LootFilter.cleanList[1]["link"].." "..LootFilter.Locale.LocText["LTWasDeleted"]..": "..LootFilter.Locale.LocText["LTItemLowestValue"].." ("..calculatedValue / 10000 ..")");
 			table.remove(LootFilter.cleanList, 1);
 		end;
 		LootFilter.schedule(LootFilter.LOOT_PARSE_DELAY, LootFilter.processCaching);				
